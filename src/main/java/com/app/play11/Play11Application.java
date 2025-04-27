fb43280b1497700d7c3dc8286e320cc6c53ef73d
imshahbaz21 <imshahbaz21@gmail.com>
2025-04-27 14:13:00 +0530

    loggers

--- src/main/java/com/app/play11/Play11Application.java
+++ src/main/java/com/app/play11/Play11Application.java
@@ -3,11 +3,15 @@ package com.app.play11;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class Play11Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Play11Application.class, args);
+		log.info("XXXXX Play11 Started Successfully XXXXX");
 	}
 
 }
